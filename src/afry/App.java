800e52c4593d0d6e3ab4999fece79a21549fc384
knpanzar <ronnie.heimer@gmail.com>
2022-04-19 15:51:23 +0200

    Add files via upload

---
+++ src/afry/App.java
@@ -0,0 +1,6 @@
+package afry;
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ src/afry/MyTest.java
@@ -0,0 +1,19 @@
+package afry;
+import static org.junit.Assert.*;  
+import org.junit.Test;  
+
+public class MyTest 
+{
+    @Test
+    public void test1() {
+        assertTrue(true);
+    }
+    @Test
+    public void test2() {
+        assertTrue(true);
+    }
+    @Test
+    public void test3() {
+        assertTrue(true);
+    }    
+}
